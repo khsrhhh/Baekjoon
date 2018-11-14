3267371836371f91f81193a826b2a17a0bd9f5a7
khsrhhh <39553283+khsrhhh@users.noreply.github.com>
2018-11-14 23:58:05 +0900

    no.10039

---
+++ 10039.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int score[5];
+    int sum = 0;
+    
+    for(int i=0; i<5; i++)
+    {
+        cin >> score[i];
+        
+        if(score[i] < 40)   score[i] = 40;
+        
+        sum += score[i];
+    }
+    
+    cout << sum/5 << endl;
+    
+    return 0;
+}
