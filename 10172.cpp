d0450ba8a5264f17ca410edccec6e6b393e1b352
khsrhhh <39553283+khsrhhh@users.noreply.github.com>
2018-11-16 18:37:07 +0900

    no.10172

---
+++ 10172.cpp
@@ -0,0 +1,23 @@
+/*
+
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    cout << "|\\_/|" << endl
+        << "|q p|   /}" << endl
+        << "( 0 )\"\"\"\\" << endl
+        << "|\"^\"`    |" << endl
+        << "||_/=\\\\__|" << endl;
+    
+    return 0;
+}
