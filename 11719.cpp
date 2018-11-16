1e646f1e5e9d5a78a362b18c76b5b7ab0a9a0ead
khsrhhh <39553283+khsrhhh@users.noreply.github.com>
2018-11-16 18:40:27 +0900

    no.11719

---
+++ 11719.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	for (int i = 0; i < 100; i++)
+	{
+		char a[100];
+
+		cin.getline(a, 101, '\n');
+		cout << a << endl;
+	}
+
+	return 0;
+}
