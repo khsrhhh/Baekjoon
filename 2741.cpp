a75ad05ea8783eedf4e43e4f53e36d5a4a2e0d97
khsrhhh <39553283+khsrhhh@users.noreply.github.com>
2018-11-18 20:14:39 +0900

    no.2741

---
+++ 2741.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(void)
+{
+    int n;
+    int sum=0;
+    scanf("%d", &n);
+    
+    for(int i=1; i<n+1; i++)
+    {
+        printf("%d\n", i);
+    }
+    
+    return 0;
+}
