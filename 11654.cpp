8fdd3a2dd6ba6b7da49a3a2116f10d5c97c4fc93
khsrhhh <39553283+khsrhhh@users.noreply.github.com>
2018-11-25 11:53:49 +0900

    no.11654

---
+++ 11654.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(void)
+{
+    char num;
+    
+    scanf("%c", &num);
+    
+    printf("%d\n", num);
+    
+    return 0;
+}
